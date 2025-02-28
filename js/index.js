a5ded81095a62a45189279319130f2d7bcb3a9b1
JiuYuanS <jiuyuansun@gmail.com>
2025-02-28 12:31:41 +0800

    add first page

--- js/index.js
+++ js/index.js
@@ -3,44 +3,55 @@ alert("Hello, World!");
 //let可以存储不同类型的值
 let time = 1;
 time = "a";
+time = true;
+time = null;
+time = undefined;
+time = {};
+time = [];
+time = function() {};
+//获取当前时间
+let now = new Date();
+time = now.toLocaleString();
 //常量用const表示，只能赋值一次
 const PI = 3.1415926;
-//条件语句
-if (time === 1) {
-  console.log("time is 1");
-} else if (time === "a") {
-  console.log("time is a");
-} else {
-  console.log("time is neither 1 nor a");
-}
-//循环语句
-for (let i = 0; i < 5; i++) {
-  console.log(i);
-}
-//数组
-let arr = [1, 2, 3, 4, 5];
-//遍历数组
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
-//对象
-let obj = {
-  name: "John",
-  age: 30,
-  city: "New York"
-};
-//遍历对象
-for (let key in obj) {
-  console.log(key + ": " + obj[key]);
-}
-//函数
-function sayHello(name) {
-  console.log("Hello, " + name + "!");
+//console.log可以输出信息到控制台;document.write可以输出信息到页面(不常用)
+console.log(PI);
+//document.write(PI); 
+//模板字符串
+let message = `welcome, ${time}!`;
+alert(message);
+//函数，具名函数
+function sayHello(time) {
+  console.log("Hello, " + time + "!");
 }
 sayHello("John");
+//匿名函数
+let sayHi = function(time) {
+  console.log("Hi, " + time + "!");
+}
+sayHi("Jane");
 //箭头函数
 let add = (a, b) => a + b;
+let sub = (a, b) => {
+    return a - b;
+}
 console.log(add(2, 3)); // 5
+//方法
+let person = {
+  name: "John",
+  sayHello: function() {
+    console.log("Hello, my name is " + this.name + "!");
+  }
+  //sayHello(){console.log("Hello, my name is " + this.name + "!");}
+  };
+person.sayHello(); // Hello, my name is John!
+//json
+let json = '{"name": "John", "age": 30, "city": "New York"}';
+let a = {name: "John", age: 30, city: "New York"};
+let b = JSON.stringify(a);//将对象转换为json字符串
+let c = JSON.parse(b);//将json字符串转换为对象
+alert(b); // {"name":"John","age":30,"city":"New York"}
+alert(c.name); // John
 //类
 class Person {
   constructor(name, age) {
